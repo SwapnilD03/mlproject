3e0561e6cbae8b38c85f817136b97527c118e35c
SwapnilD03 <swapnildas18403@gmail.com>
2025-07-31 16:41:49 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -29,9 +31,9 @@ class DataIngestion:
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Ingestion of the data is completed")
 
@@ -45,4 +47,8 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()        
+    train_data,test_data=obj.initiate_data_ingestion()        
+
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
